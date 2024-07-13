6cd85b1f59e7267869a77f7ac8369b9e1035cd8a
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2024-07-13 16:06:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var num1 = 7;
+console.log(num1);
+
+let str = "seven";
+console.log(str);
+
+const yes = true;
+const no = false;
+
+console.log(yes+" "+no);
+
+let number = 77;
+let string = "string";
+let boolean = true;
+let object = {
+    name: "forty nine",
+    age: 49,
+    isOld: true
+}
+let array = [1,2,3,4,5,6,7];
+
+console.log(number+" is "+typeof(number));
+console.log(string+" is "+typeof(string));
+console.log(boolean+" is "+typeof(boolean));
+console.log(object+" is "+typeof(object));
+
+//Array is considered as an object in JS.
+console.log(array+" is "+typeof(array));
